c636d788a8623d5e8138d6510662e5adb274fbb3
Activejjhhh <117743416+Activejjhhh@users.noreply.github.com>
2023-10-14 12:25:41 +0100

    Update map.py

--- map.py
+++ map.py
@@ -1,11 +1,12 @@
-import folium
+from flask import Flask, render_template, request
 from geopy.geocoders import Nominatim
 from geographiclib.geodesic import Geodesic
+import folium
 
-# initialize the geopy geocoder
+
 geolocator = Nominatim(user_agent="geopy_example")
 
-# function to get coordinates from a city name
+
 def get_coordinates(city_name):
     location = geolocator.geocode(city_name)
     if location:
@@ -14,30 +15,40 @@ def get_coordinates(city_name):
         print(f"Coordinates not found for {city_name}")
         return None
 
-# get start and end city names from the user
-start_city = input("Enter the start city: ")
-end_city = input("Enter the end city: ")
+app = Flask(__name__)
 
-# get coordinates for the start and end cities
-start = get_coordinates(start_city)
-end = get_coordinates(end_city)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        start_city = request.form.get('start-city')
+        end_city = request.form.get('end-city')
 
-# create a new Folium map centered on the start location
-m = folium.Map(location=start, zoom_start=2)
+        # get coordinates for the start and end cities
+        start = get_coordinates(start_city)
+        end = get_coordinates(end_city)
 
-# add markers for the start and end locations
-folium.Marker(start, popup=start_city).add_to(m)
-folium.Marker(end, popup=end_city).add_to(m)
+   
+        m = folium.Map(location=start, zoom_start=2)
 
-# calculate a series of intermediate points on the geodesic path
-num_points = 100
-geod = Geodesic.WGS84  
-g = geod.Inverse(start[0], start[1], end[0], end[1])
+   
+        folium.Marker(start, popup=start_city).add_to(m)
+        folium.Marker(end, popup=end_city).add_to(m)
 
-points = [geod.Direct(g['lat1'], g['lon1'], g['azi1'], i * g['s12'] / num_points) for i in range(num_points+1)]
+        
+        num_points = 100
+        geod = Geodesic.WGS84  # define the WGS84 ellipsoid
+        g = geod.Inverse(start[0], start[1], end[0], end[1])
 
-# Add the geodesic path to the map
-folium.PolyLine([(point['lat2'], point['lon2']) for point in points], color="red", weight=2.5, opacity=1).add_to(m)
+        points = [geod.Direct(g['lat1'], g['lon1'], g['azi1'], i * g['s12'] / num_points) for i in range(num_points+1)]
 
-# Display the map
-m.save('maps.html')
+        # dd the geodesic path to the map
+        folium.PolyLine([(point['lat2'], point['lon2']) for point in points], color="red", weight=2.5, opacity=1).add_to(m)
+
+        #save map to file
+        m.save(r'C:/Users/Danie/OneDrive/Desktop/Sublime Coding/Nuclear/static/map.html')
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
