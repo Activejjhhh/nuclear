29fa09dbbe0abbfc39ac88a58ee00684237439be
Activejjhhh <117743416+Activejjhhh@users.noreply.github.com>
2023-12-22 00:23:07 +0000

    Update and rename flightc.py to icbm.py

--- icbm.py
+++ icbm.py
@@ -20,11 +20,11 @@ class ICBM:
         self.stage2 = stage2
         self.stage3 = stage3
         self.air_density = 1.225  # kg/m^3 at sea level
-        self.drag_coefficient = 0.47  # for a sphere
+        self.drag_coefficient = 0.47 
 
     def calculate_drag_force(self, velocity, altitude):
         # Adjust air density based on altitude
-        adjusted_air_density = self.air_density * math.exp(-altitude / 8000)  # Scale height of Earth's atmosphere is approximately 8000m
+        adjusted_air_density = self.air_density * math.exp(-altitude / 8000)  
         return 0.5 * self.drag_coefficient * adjusted_air_density * self.cross_section * velocity**2
 
     def calculate_acceleration(self, thrust, drag_force):
@@ -55,10 +55,7 @@ class ICBM:
             if velocity > self.max_speed:
                 velocity = self.max_speed
 
-            # Calculate distance traveled during this time step
             distance += initial_velocity + 0.5 * acceleration
-
-            # Update time and altitude
             time += 1
             altitude += velocity
 
@@ -73,46 +70,80 @@ class ICBM:
         lon1_rad = math.radians(lon1)
         lat2_rad = math.radians(lat2)
         lon2_rad = math.radians(lon2)
-
-        # Differences
         dlon = lon2_rad - lon1_rad
         dlat = lat2_rad - lat1_rad
 
         # Haversine formula
         a = math.sin(dlat / 2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2)**2
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-        # Distance in kilometers
         distance = R * c
-
-        # Time = Distance/Speed
         time = distance / speed
 
         return time
 
-minuteman_III = ICBM(
-    name="Minuteman III",
-    payload=300,
-    max_range="14,000 km",
-    max_altitude="1,126 km",
-    fuel_type="Solid",
-    mirvs="Yes",
-    payload_type="Nuclear",
-    cross_section=1.68,
-    weight_missile=36000,
-    weight_fuel=0,
-    max_speed=24000,
-    origin="USA",
-    year=1961,
-    blast_height=None,
-    stage1 = "99790",
-    stage2= "27487",
-    stage3= "15422",
-)
-launch_lat = float(input("Enter the latitude of the launch location: "))
-launch_lon = float(input("Enter the longitude of the launch location: "))
-target_lat = float(input("Enter the latitude of the target location: "))
-target_lon = float(input("Enter the longitude of the target location: "))
-time = minuteman_III.calculate_flight_time(minuteman_III.max_speed, launch_lat, launch_lon, target_lat, target_lon)
 
-print(f"The flight time of the missile is approximately {time} hours.")
+
+    def calculate_speeds_with_resistance(self, final_time):
+        time = 0
+        velocity = 0
+        distance = 0
+        altitude = 0
+        total_distance = float(self.max_range.replace(",", "").split(" ")[0])  # remove commas before converting to float
+        speeds = []  # List to store the speed at each second
+
+        while time < final_time:
+            # Adjust thrust based on the stage of the journey
+            if distance < total_distance / 3:
+                thrust = float(self.stage1)
+            elif distance < 2 * total_distance / 3:
+                thrust = float(self.stage2)
+            else:
+                thrust = float(self.stage3)
+
+            drag_force = self.calculate_drag_force(velocity, altitude)
+            acceleration = self.calculate_acceleration(thrust, drag_force)
+            initial_velocity = velocity
+            velocity += acceleration
+
+            if velocity > self.max_speed:
+                velocity = self.max_speed
+
+            speeds.append((time, velocity))  # Store the time and speed at this second
+
+            if distance < total_distance:
+                distance += initial_velocity + 0.5 * acceleration
+                altitude += velocity
+
+            #print(f"At time {time} seconds, the speed is {velocity} m/s, distance is {distance} m, acceleration is {acceleration} m/s^2, and thrust is {thrust} N.")
+            time += 1
+
+        return speeds, velocity, time   # Return the list of speeds
+
+
+
+
+
+# Create a list of ICBM instances
+missiles = [
+    ICBM(
+        name="Minuteman III",
+        payload=300,
+        max_range="14,000 km",
+        max_altitude="1,126 km",
+        fuel_type="Solid",
+        mirvs="Yes",
+        payload_type="Nuclear",
+        cross_section=1.68,
+        weight_missile=36000,
+        weight_fuel=0,
+        max_speed=114720,  #have to times by 4.78 for some reason
+        origin="USA",
+        year=1961,
+        blast_height=None,
+        stage1 = "890000",
+        stage2= "260000",
+        stage3= "151000", #in newtons
+    ),
+    # Add more ICBM instances here...
+]
+
